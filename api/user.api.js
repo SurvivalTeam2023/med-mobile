5e9872ef3401fd034e8c0abeb6d1027ef35347ed
tamphamfptu <tampham03112@gmail.com>
2023-02-01 10:30:44 +0700

    refactor chooseMusicScreen flow

--- api/user.api.js
+++ api/user.api.js
@@ -3,7 +3,6 @@ import { store } from "../core/store/store";
 
 export const getUserByNameApi = (payload) => {
   const userName = store.getState().user.username;
-  console.log("ngu qua");
   const queryParam = "/" + `${userName}`;
   const url = "/user" + `${queryParam}`;
   return CallAPI.get(url);
